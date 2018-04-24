58bd4ac903840dddfa4431d678c1ce59393c597e
MD Nur <mdnur701@gmail.com>
2018-04-24 14:46:12 +0000

    Handler.php edited online with Bitbucket Addded unauthenticated function in handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,4 +50,23 @@ class Handler extends ExceptionHandler
     {
         return parent::render($request, $exception);
     }
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+
+
+        $guard = array_get($exception->guards(),0);
+        switch ($guard) {
+            case 'admin':
+                return $request->expectsJson()
+                    ? response()->json(['message' => $exception->getMessage()], 401)
+                    : redirect()->guest(route('admin.login'));
+                break;
+
+            default:
+                return $request->expectsJson()
+                    ? response()->json(['message' => $exception->getMessage()], 401)
+                    : redirect()->guest(route('login'));
+                break;
+        }
+    }
 }
